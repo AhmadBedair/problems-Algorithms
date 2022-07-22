a56889c3e2c6f703263965abca124c46b333878f
Ahmad Bedair <ahmadbedair@Ahmads-MacBook-Pro.local>
2022-07-22 20:27:10 +0200

    Solution Uploaded

---
+++ leetCode/dp/PalindromicSubstrings.java
@@ -0,0 +1,54 @@
+// Palindromic Substrings
+// Given a string s, return the number of palindromic substrings in it.
+
+// A string is a palindrome when it reads the same backward as forward.
+
+// A substring is a contiguous sequence of characters within the string.
+
+ 
+
+// Example 1:
+
+// Input: s = "abc"
+// Output: 3
+// Explanation: Three palindromic strings: "a", "b", "c".
+// Example 2:
+
+// Input: s = "aaa"
+// Output: 6
+// Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 1000
+// s consists of lowercase English letters.a
+class Solution {
+    public int countSubstrings(String s) {
+        int out = s.length();
+        int dp[][] = new int[s.length()][s.length()];
+        
+        for(int i=0;i<s.length();i++){
+            dp[i][i] = 1;
+        }
+        
+        for(int col=1;col<s.length();col++){
+            for(int row = 0;row<col;row++){
+                if(col==row+1&&s.charAt(col)==s.charAt(row)){
+                    out++;
+                    dp[row][col]=1;
+                }
+                else if(col>row+1&&s.charAt(col)==s.charAt(row)&&dp[row+1][col-1]==1){
+                     out++;
+                    dp[row][col]=1;
+                    
+                }
+              
+                
+            }
+        }
+        return out;
+        
+        
+    }
+}
